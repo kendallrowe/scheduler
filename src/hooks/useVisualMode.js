c174dd4da32143f90b9a813a39ed9da38cf86389
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-08 09:55:18 -0400

    Implemented back function for custom useVisual hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,22 +2,26 @@ import { useState } from "react";
 
 const useVisualMode = (FIRST) => {
   const [mode, setMode] = useState(FIRST);
-  const [history, setHistory] = useState(mode);
+  const [history, setHistory] = useState([FIRST]);
 
   const transition = (SECOND) => {
     setMode(SECOND);
-    setHistory(mode)
+    setHistory(prev => ([...prev, SECOND]));
   };
 
-  
 
   const back = () => {
-
+    if (history.length !== 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
   };
+  
 
   return { 
     mode,
-    transition 
+    transition,
+    back 
   };
 };
 
