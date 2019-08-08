e520955424893d4ee9badcbe8bdb73c097a7485d
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-08 13:13:46 -0400

    Implement visual hook to render empty components on main application index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,11 +3,23 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import { useVisualMode } from "../../hooks/useVisualMode";
+const  EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+  const { mode } = useVisualMode(EMPTY);
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
+      {mode === EMPTY && <Empty />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {/* <Header time={props.time}/>
       {!props.interview && <Empty
         onAdd={e => console.log("clicked")}
       />
@@ -17,7 +29,7 @@ export default function Appointment(props) {
         interviewer={props.interview.interviewer}
         onEdit={e => console.log("edited")}
         onDelete={e => console.log("deleted")}
-      />}
+      />} */}
     </article>
   );
 }
