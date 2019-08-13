b76d5b66f6a9a1c22b70d796bbb8958abc4c5a4d
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-13 20:35:39 +0000

    Created create appointment and edit appointment tests in appointment.spec.js for cypress

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,19 +1,56 @@
-import { checkPropTypes } from "prop-types";
-
 describe("Appointments", () => {
-  // Load page and confirm rendered waiting for "Monday"
-  it("should book an interview", () => {
-    cy.visit("/");
-    cy.containers("[data-testid=day]", "Monday");
+  beforeEach(() => {
+    cy.request("POST", "http://localhost:3001/api/debug/reset")
 
+    // Load page and confirm rendered waiting for "Monday" 
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Monday");
+   });
+
+  it("should book an interview", () => {
     // Clicks on the "Add" button in second appointment
+    cy.get("[alt='Add']")
+    .first()
+    .click()
+  
+    // After click, enter name into input field
+    cy.get("[data-testid=student-name-input").type("Lydia Miller-Jones");
+
+    // Choose an interviewer
+    cy.get("[alt='Sylvia Palmer']")
+    .click()
+
+    // Click the save button
+    cy.contains("Save")
+    .click()
+
+    // Confirm booked appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
-  // it("should navigate to Tuesday", () => {
-  //   cy.visit("/");
-  //   cy.contains("[data-testid=day]", "Tuesday").click()
-  //   .should('have.class', "day-list__item--selected");
+  it("should edit an interview", () => {
+    // Clicks on the "Add" button in second appointment
+    cy.get("[alt='Edit']")
+    .first()
+    .click({ force: true })
+    
+    // After click, enter name into input field
+    cy.get("[data-testid=student-name-input")
+    .clear()
+    .type("Kendall Rowe");
 
+    // // Choose an interviewer
+    cy.get("[alt='Tori Malcolm']")
+    .click()
 
-  // });
+    // // Click the save button
+    cy.contains("Save")
+    .click()
+
+    // // Confirm booked appointment
+    cy.contains(".appointment__card--show", "Kendall Rowe");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
