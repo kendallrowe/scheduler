8e99fa981a71cc98156b0c07dd50b0535a7d3b96
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-15 13:19:34 +0000

    Further websocket configuration

--- src/index.js
+++ src/index.js
@@ -5,4 +5,13 @@ import "index.scss";
 
 import Application from "components/Application";
 
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+} else {
+  axios.defaults.baseURL = "http://localhost:3001";
+}
+
+
 ReactDOM.render(<Application />, document.getElementById("root"));
