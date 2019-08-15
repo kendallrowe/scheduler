0074c2903064915e80400bbbb0c83ea64b6840e6
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-15 14:38:47 +0000

    Configuring netlify.toml

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-} else {
-  axios.defaults.baseURL = "http://localhost:3001";
-}
-
+} 
+console.log(axios.defaults.baseURL);
 
 ReactDOM.render(<Application />, document.getElementById("root"));
